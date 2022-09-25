bb8f7319cd643b608f6bb99f6fc33ebdfea36db2
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-09-25 20:14:32 +0600

    Adding http Dependencies.

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:jsontest/ui/json_parsing.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: JsonParsingSimple(),
+  ));
+}
+
+
---
+++ lib/ui/json_parsing.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JsonParsingSimple extends StatefulWidget {
+  const JsonParsingSimple({Key? key}) : super(key: key);
+
+  @override
+  State<JsonParsingSimple> createState() => _JsonParsingSimpleState();
+}
+
+class _JsonParsingSimpleState extends State<JsonParsingSimple> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
